8d7fc46c3f7a8d6b147a0fe45a171ee45c6e3de5
haesooKr <gotn676@gmail.com>
2020-04-28 19:48:35 -0400

    Use hbs view engine

--- server.js
+++ server.js
@@ -12,10 +12,25 @@ const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-ac
 
 const app = express();
 
+const thread = require('./controllers/thread');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ 
+  extname: 'hbs', 
+  defaultLayout: 'main', 
+  layoutsDir: __dirname + '/views/layouts/', 
+  handlebars: allowInsecurePrototypeAccess(handlebars)
+}));
+app.set('view engine', 'hbs');
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
 
-app.get('/', (req, res) => {
-  res.send('Server HomePage')
-})
+app.use('/', thread);
+
