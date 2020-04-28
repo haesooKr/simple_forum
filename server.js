a79673edd42b6b87629a36110a29dd5e5928b091
haesooKr <gotn676@gmail.com>
2020-04-28 08:05:24 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const express = require('express');
+const path = require('path');
+const handlebars = require('handlebars');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+  res.send('Server HomePage')
+})
