5eee62db742d982bf9014734fcbcf0fd30253763
haesooKr <gotn676@gmail.com>
2020-04-28 20:39:58 -0400

    Add monthToNum function

--- controllers/thread.js
+++ controllers/thread.js
@@ -4,13 +4,48 @@ const connection = require('../models/db');
 
 router.get('/', (req, res) => {
   connection.query('SELECT * FROM THREAD;', (err, result) => {
-    console.log(JSON.stringify(result))
+    for(let i=0; i<result.length; i++){
+      let [day, month, date, year] = result[i].INS_DATE.toString().split(' ');
+      result[i].INS_DATE = year + '/' + monthToNum(month) + '/' + date
+    }
 
     res.render('layouts/forum', {
       thread: result,
-      style: "/css/main"
+      style: "/css/thread"
     })
   })
 });
+
+function monthToNum(month){
+  switch(month){
+    case 'Jan':
+      return '1';
+    case 'Feb':
+      return '2';
+    case 'Mar':
+      return '3';
+    case 'Apr':
+      return '4';
+    case 'May':
+      return '5';
+    case 'Jun':
+      return '6';
+    case 'Jul':
+      return '7';
+    case 'Aug':
+      return '8';
+    case 'Sep':
+      return '9';
+    case 'Oct':
+      return '10';
+    case 'Nov':
+      return '11';
+    case 'Dec':
+      return '12';
+    default :
+      return new Error('Wrong Format of Month Input.')
+  }
+}
+
 
 module.exports = router;
