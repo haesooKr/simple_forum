e1219b01799c343328ed0649b6e769fac91d6ca3
haesooKr <gotn676@gmail.com>
2020-05-03 23:27:30 -0400

    Add limitations for lengths

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,13 +3,17 @@ const nickname = document.querySelector('input[name=nickname]');
 const comment = document.querySelector('input[name=comment]');
 submitBtn.addEventListener('click', (e) => {
   let condition = true;
-  if(nickname.value.length < 3){
+  if(nickname.value.length < 3 || nickname.value.length > 10){
     condition = false;
-    alert('닉네임은 3글자 이상이여야 합니다.');
+    alert('닉네임은 3글자 이상, 10글자 이하여야 합니다.');
   }
   if(!comment.value){
     condition = false;
     alert('댓글 내용을 입력해주시기 바랍니다.');
+  }
+  if(comment.value.length > 50){
+    condition = false;
+    alert('댓글 내용을 50자 이하로 입력해주시기 바랍니다.')
   }
   if(condition === false){
     e.preventDefault();
