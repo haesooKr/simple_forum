821a17adbca9b27517fc5de19fb8a2fc5a21faf0
haesooKr <gotn676@gmail.com>
2020-05-04 17:12:26 -0400

    Update limitation

--- public/js/comment.js
+++ public/js/comment.js
@@ -3,17 +3,13 @@ const nickname = document.querySelector('input[name=nickname]');
 const comment = document.querySelector('input[name=comment]');
 submitBtn.addEventListener('click', (e) => {
   let condition = true;
-  if(nickname.value.length < 3 || nickname.value.length > 10){
+  if(nickname.value.length < 2 || nickname.value.length > 10){
     condition = false;
-    alert('닉네임은 3글자 이상, 10글자 이하여야 합니다.');
+    alert('닉네임은 2글자 이상, 10글자 이하로 입력해주시기 바랍니다.');
   }
-  if(!comment.value){
+  if(comment.value.length < 2 || comment.value.length > 50){
     condition = false;
-    alert('댓글 내용을 입력해주시기 바랍니다.');
-  }
-  if(comment.value.length > 50){
-    condition = false;
-    alert('댓글 내용을 50자 이하로 입력해주시기 바랍니다.')
+    alert('댓글 내용을 2글자 이상, 50자 이하로 입력해주시기 바랍니다.')
   }
   if(condition === false){
     e.preventDefault();
